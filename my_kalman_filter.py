511f5a7a8f0c0048338583e80778e5c6862cbb0d
yiğit cılboğlu <149807279+yigitcilb@users.noreply.github.com>
2025-02-06 12:57:01 +0300

    Update my_kalman_filter.py

--- my_kalman_filter.py
+++ my_kalman_filter.py
@@ -6,29 +6,37 @@ class KalmanFilter:
         self.Q = np.array([[Q, 0, 0],
                            [0, Q, 0],
                            [0, 0, Q]])
-        self.B = np.array([[(delta_t * delta_t)/2, delta_t, 1]])
-        self.R = np.array([[R]])  
+        self.B = np.array([[(delta_t**2)/2], [delta_t], [1]])
+        self.R = np.array([[R, 0, 0],
+                           [0, R, 0],
+                           [0, 0, R]]) 
         self.delta_t = delta_t
         self.F = np.array([[1, delta_t, 0],
                            [0, 1, delta_t],
                            [0, 0, 1]])
-        self.P = np.eye(3)
-        self.H = np.array([[0,0,1]])  
-        self.fin = np.array([[1,0,0]])
+        self.P = np.eye(3) * 1000
+        self.H = np.array([[1,delta_t,0], [0,1,delta_t], [0,0,1]])
 
-        self.z_hat = np.array([[0]])  
+        self.fin = np.array([[1], [0], [0]]) 
+        self.pos_hat = 0
+
+        self.z_hat = np.array([[0], [0],[0]])  
         self.x_hat = np.array([[0], [0], [0]]) 
         self.speed_hat = 0
 
     def update_filter(self, imu: float) -> float:
+
         self.x_hat_minus = np.dot(self.F, self.x_hat) + np.dot(self.B, imu)
         self.P_minus = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
         S = np.dot(np.dot(self.H, self.P_minus), self.H.T) + self.R  
         self.K = np.dot(np.dot(self.P_minus, self.H.T), np.linalg.inv(S)) 
-        z = imu
-        z = np.array([0, 0, z]) 
+        z = np.array([[self.pos_hat + imu*(self.delta_t**2)/2],[self.speed_hat + imu* self.delta_t], [imu]])
         self.v_k = z - np.dot(self.H, self.x_hat_minus)
-        self.x_hat = self.x_hat_minus + np.dot(self.K, self.v_k)  
-        self.P = np.dot(np.eye(3) - np.dot(self.K, self.H), self.P_minus)  
+        self.x_hat = self.x_hat_minus + np.dot(self.K, self.v_k) 
+        Y = np.eye(3) - np.dot(self.K, self.H)
+        self.P = np.dot(np.dot(Y, self.P_minus), Y.T)  + np.dot(np.dot(self.K, self.R), self.K.T)
         self.z_hat = np.dot(self.H, self.x_hat)  
-        return np.dot(self.fin, self.x_hat)[0]
+        print(self.x_hat)
+        self.speed_hat = self.x_hat[1,0]
+        self.pos_hat = self.x_hat[0,0]
+        return self.pos_hat
